86f1e9c6cb5c03716e7964408b94f3eca866aa16
AlexandrDeb <alexanderpgs@ukr.net>
2017-08-31 15:04:12 +0300

    complite 1.0

--- task2.php
+++ task2.php
@@ -1,44 +1,63 @@
 <?php
-abstract class Tariff {
+
+abstract class Tariff
+{
 
     protected $value;
+
     public function getTariff()
     {
         return $this->value;
     }
 }
-abstract class Lessons {
+
+abstract class Lessons
+{
     private $tariff;
-    private $quantity;
-    public function __construct($tariff, $quantity)
+    private $amount;
+
+    public function __construct($tariff, $amount)
     {
-        $this->tariff  = $tariff;
-        $this->quantity = $quantity;
+        $this->tariff = $tariff;
+        $this->amount = $amount;
     }
+
     public function getCost()
     {
-        echo $this->tariff  * $this->quantity;
+        echo $this->tariff * $this->amount;
     }
 
 }
-class Speaking extends Lessons {}
-class Grammar extends Lessons {}
-class Fixed extends Tariff {
+
+class Speaking extends Lessons
+{
+}
+
+class Grammar extends Lessons
+{
+}
+
+class Fixed extends Tariff
+{
     protected $value = 200;
 }
-class Hourly extends Tariff {
+
+class Hourly extends Tariff
+{
     protected $value = 100;
 }
-class Factory {
+
+class Factory
+{
 
     public static function make($type)
     {
         $className = $type['type'];
 
-        if(class_exists($type['tariff']) && class_exists($className)) {
+        if (class_exists($type['tariff']) && class_exists($className)) {
 
             $tariff = new $type['tariff'];
-            return new $className($tariff->getTariff(), $type['quantity']);
+            return new $className($tariff->getTariff(), $type['amount']);
 
         } else {
 
@@ -47,6 +66,7 @@ class Factory {
 
     }
 }
+
 class LessonController
 {
     public function create($type)
@@ -55,25 +75,26 @@ class LessonController
         return $product;
     }
 }
+
 $typeLessons = array(
     "speaking" => array(
-        'type'  => 'Speaking',
+        'type' => 'Speaking',
         'tariff' => 'fixed',
-        'quantity' => 2
+        'amount' => 2
     ),
     "grammar" => array(
-        'type'  => 'Grammar',
+        'type' => 'Grammar',
         'tariff' => 'hourly',
-        'quantity' => 1
+        'amount' => 1
     )
 );
 $lesson = new LessonController();
-foreach($typeLessons as $key=>$type) {
+foreach ($typeLessons as $key => $type) {
     try {
         $obj[] = $lesson->create($type);
-    } catch(Exception $e) {
+    } catch (Exception $e) {
         echo $e->getMessage();
     }
 }
 
-print_r($obj);
+var_dump($obj);
